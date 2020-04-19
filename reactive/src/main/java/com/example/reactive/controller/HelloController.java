3313f9b36b8a78b9972f8f00949f3f0d6b93b770
yifan <1205791932@qq.com>
2020-04-20 05:15:26 +0800

    flux

--- reactive/src/main/java/com/example/reactive/controller/HelloController.java
+++ reactive/src/main/java/com/example/reactive/controller/HelloController.java
@@ -2,7 +2,13 @@ package com.example.reactive.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class HelloController {
@@ -11,4 +17,15 @@ public class HelloController {
     public Mono<String> getUser(){
         return Mono.just("l am alive");
     }
+
+    @GetMapping("/flux")
+    public Flux<String> getFlux(){
+        List<String > list = new ArrayList<>();
+        for (int a= 0;a < 10;a++) {
+            list.add("hello");
+        }
+        return Flux.fromIterable(list);
+
+    }
+
 }
